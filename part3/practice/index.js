2ac7482ce2805b476aa1ce78a54c3a6aa9bec7c5
boylek42 <boylekevin2000@gmail.com>
2025-07-23 15:21:57 +0100

    For my MongoDB Database, I've created a note model for my application within the /models directory

--- part3/practice/index.js
+++ part3/practice/index.js
@@ -1,93 +1,123 @@
-const express =  require('express')
+require('dotenv').config()
+const express = require('express')
+const Note = require('./models/note')
+
 const app = express()
 
-// Middleware: Static - serve static files such as HTML, CSS, JS files and images in Express.
-//     - Now, whenever Expess receives a GET request it will check if the dist directory contains a 
-//       corresponding file - if so - it will return it.
-app.use(express.static('dist'))
+let notes = []
 
-app.use(express.json())
-
-let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-    },
-
-    {
-        id: "2",
-        content: "CSS is medium",
-        important: false
-    },
-
-    {
-        id: "3",
-        content: "JS is tough",
-        important: true
-    },
-]
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
-})
-
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if(note) {
-        response.json(note)   
-    }
-    else {
-        response.statusMessage = "Note does not exist"
-        response.status(404).end()
-    }
-})
-
-// Delete an existing note using the HTML DELETE Method
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
-})
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(note => Number(note.id)))
-        : 0
-
-        console.log(maxId)
-        return String(maxId + 1)
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
 
-// Create new note using the HTML POST Method
-app.post('/api/notes', (request, response) => {
-    const body = request.body
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-    // Calling return here is crucial, if we don't the empty note still get's
-    // added as the rest of this route executes.
-    if (!body.content) {
-        return response.status(404).json({
-            error: 'Content Missing!'
-        })
-    }
+  if(error.name === 'CastError') {
+    return response.status(400).send( {error: 'Malformatted ID'} )
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json( {error: error.message} )
+  }
+  next(error)
+  // else if (error.name == 'TypeError') {
+  //   return response.status(400).send({error : 'Invalid Object ID'})
+}
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: String(generateId(notes))
-    }
 
-    // notes = notes.concat(note)
-    response.json(note)
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
-const PORT = process.env.PORT || 3001
+// Find all users within the DB
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes)
+  })
+})
+
+// Find user by ID.
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then((note) => {
+    if(note) {
+      response.json(note)
+    }
+    else {
+      response.status(404).end()
+    }
+  })
+  // Prints error via error handling middleware.
+  .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  // Create a new note from the Note model: 
+    // it's content is given in the request.body
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  // Save the new note and return it to the client in JSON format.
+  note.save()
+  .then((savedNote) => {
+    response.json(savedNote)
+  })
+  .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+  .then(note => {
+    console.log(`Removed ${note.content} from contacs.`)
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const {content, important} = request.body
+  
+  // Find the note with the ID given in request.
+  Note.findById(request.params.id).then(note => {
+    // Return error if not found.
+    if(!note) {
+      response.status(404).end()
+    }
+
+    // Update the note’s content and importance with values from the request body.
+    note.content = content
+    note.important = important
+
+    // Save the updated note and return it in JSON format.
+    note.save().then(updatedNote => {
+      return response.json(updatedNote)
+    })
+  }).catch(error => next(error))  
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+
+// Middleware for after HTTP Routes have been defined:
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server is now running on port: ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
